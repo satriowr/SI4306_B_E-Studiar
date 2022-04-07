c67c55ee43595d4438217ac2ce95ec4d5b2a9249
satriowr <iam.satriorahman28@gmail.com>
2022-04-07 11:14:38 +0700

    Develop Landing Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,87 +1,318 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US" dir="ltr">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Estudiar</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-  </script>
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Estudiar | Solusi Jadi Pintar</title>
 
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-  </script>
 
-  <style>
-    body {
-      font-family: 'Poppins', sans-serif;
-    }
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.png">
+  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+  <meta name="theme-color" content="#ffffff">
 
-    .container-navbar {
-      width: 160px;
-      height: 33px;
-      background-color: #1780E2;
-      border-radius: 10000px;
-      padding-left: 23px;
-      padding-top: 3px;
-      padding-bottom: 5px;
-      margin-left: 12px;
-    }
 
-    .container-navbar a {
-      font-size: 16px;
-      color: white;
-      text-decoration: none;
-
-    }
-  </style>
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="assets/css/theme.css" rel="stylesheet" />
 
 </head>
 
+
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container-fluid d-flex justify-content-beetween" style="margin-top: 30px>
-      <div class=" image">
-      <a class="navbar-brand" href="#">
-        <img src="https://i.ibb.co/6yTVSfP/image-1.png" alt="" width="100px" height="43px">
-      </a>
-    </div>
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main" id="top">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top" data-navbar-on-scroll="data-navbar-on-scroll">
+      <div class="container"><a class="navbar-brand" href="/"><img src="https://i.ibb.co/6yTVSfP/image-1.png"
+            height="50" alt="logo" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+            class="navbar-toggler-icon"> </span></button>
+        <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="#feature">Layanan</a></li>
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="#validation">Cara Kami Bekerja</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="#superhero">Berikan Kursus</a></li>
+            <li class="nav-item"><a class="nav-link" aria-current="page" href="#marketing">Ulasan</a></li>
+          </ul>
+          <div class="d-flex ms-lg-4">
+            <a class="btn btn-secondary-outline" href="#!">Masuk</a>
+            <div class="button-signup">
+              <button type="button" style="background-color: #1780E2; color:white; margin-left:8px"
+                class="btn">Daftar</button>
+            </div>
 
-    <div class="navi" id="navbarNav" style="font-size: 18px; color:#252641">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <div class="container-navbar">
-            <a href="?">Tentang Kami</a>
           </div>
-        </li>
-
-        <li class="nav-item">
-          <div class="container-navbar">
-            <a href="?">Berikan Kursus</a>
+        </div>
+      </div>
+    </nav>
+    <section class="pt-7">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 text-md-start text-center py-6">
+            <h1 class="mb-4 fs-9 fw-bold">Jadi <span style="color: #FFBD07">Pintar</span> Bersama <span
+                style="color: #1780E2">Estudiar</span></h1>
+            <p class="mb-6 lead text-secondary">Cari mentor privat untuk <span style="font-weight: bold">TK/PAUD, SD,
+                SMP, SMA, Universitas dan Umum.</span> Kualitas pengajar terjamin dan berpengalaman di bidangnya. Kamu
+              juga bisa atur kebutuhan belajarmu.</p>
+            <div class="text-center text-md-start"><a class="btn btn-warning me-3 btn-lg" href="#!" role="button">Mulai
+                belajar</a></div>
           </div>
-        </li>
+          <div class="col-md-6 text-end"><img class="pt-7 pt-md-0 img-fluid" src="assets/img/hero/hero-img.png"
+              alt="" /></div>
+        </div>
+      </div>
+    </section>
 
-      </ul>
-    </div>
-    </div>
-  </nav>
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="pt-5 pt-md-9 mb-6" id="feature">
+
+      <div class="bg-holder z-index--1 bottom-0 d-none d-lg-block"
+        style="background-image:url(assets/img/category/shape.png);opacity:.5;">
+      </div>
+      <!--/.bg-holder-->
+
+      <div class="container">
+        <h1 class="fs-9 fw-bold mb-4 text-center">Mencari mentor privat <br class="d-none d-xl-block" />Tidak pernah
+          semudah ini</h1>
+        <div class="row">
+          <div class="col-lg-3 col-sm-6 mb-2"> <img class="mb-3 ms-n3" src="assets/img/category/icon1.png" width="75"
+              alt="Feature" />
+            <h4 class="mb-3">Pilih mentor kamu</h4>
+            <p class="mb-0 fw-medium text-secondary">Cek profil mentor dengan bebas dan sesuaikan dengan kebutuhan</p>
+          </div>
+          <div class="col-lg-3 col-sm-6 mb-2"> <img class="mb-3 ms-n3" src="assets/img/category/icon2.png" width="75"
+              alt="Feature" />
+            <h4 class="mb-3">Ajukan permintaan</h4>
+            <p class="mb-0 fw-medium text-secondary">Mentor merespon permintaan kursus kamu dalam beberapa jam</p>
+          </div>
+          <div class="col-lg-3 col-sm-6 mb-2"> <img class="mb-3 ms-n3" src="assets/img/category/icon3.png" width="75"
+              alt="Feature" />
+            <h4 class="mb-3">Atur jadwal sesukamu</h4>
+            <p class="mb-0 fw-medium text-secondary">Atur pelaksanaan dan jadwal kursus </p>
+          </div>
+          <div class="col-lg-3 col-sm-6 mb-2"> <img class="mb-3 ms-n3" src="assets/img/category/icon4.png" width="75"
+              alt="Feature" />
+            <h4 class="mb-3">Mentor menunggumu </h4>
+            <p class="mb-0 fw-medium text-secondary">Mulai belajar dengan mentor pilihamu</p>
+          </div>
+        </div>
+        <br>
+        <div class="text-center"><a class="btn btn-warning" href="#!" role="button">Bergabung Sekarang</a></div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
 
 
+
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="pt-5" id="validation">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h5 class="text-secondary">Kami memiliki banyak</h5>
+            <h2 class="mb-2 fs-7 fw-bold">Mentor yang profesional</h2>
+            <p class="mb-4 fw-medium text-secondary">
+              Estudiar akan membantumu untuk menemukan mentor yang telah terverifikasi dan berpengalaman di bidangnya
+            </p>
+            <h4 class="fs-1 fw-bold">Lakukan pembelajaran offline</h4>
+            <p class="mb-4 fw-medium text-secondary">Kamu dapat melakukan
+              pembelajaran secara langsung. Namun, wajib mengikuti protokol kesehatan yang telah ditetapkan oleh
+              pemerintah.
+            </p>
+            <h4 class="fs-1 fw-bold">Rekomendasi Mentor</h4>
+            <p class="mb-4 fw-medium text-secondary">Setiap siswa yang belajar di Estudiar tetap dibimbing oleh mentor
+              yang telah melewati serangkaian proses rekrutmen yang ketat dan diseleksi secara khusus sebelum
+              direkomendasikan</p>
+            <h4 class="fs-1 fw-bold">Tanyakan masalahmu kepada mentor</h4>
+            <p class="mb-4 fw-medium text-secondary">Mentor kami akan membantumu belajar untuk menyelesaikan masalah
+              tugas dengan profesional</p>
+          </div>
+          <div class="col-lg-6"><img class="img-fluid" src="assets/img/validation/validation.png" alt="" /></div>
+        </div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="pt-5" id="manager">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6"><img class="img-fluid" src="assets/img/manager/manager.png" alt="" /></div>
+          <div class="col-lg-6">
+            <h5 class="text-secondary">Pelayanan terbaik untuk anda</h5>
+            <p class="fs-7 fw-bold mb-2">Layanan pelanggan Estudiar</p>
+            <p class="mb-4 fw-medium text-secondary">
+              Untuk menunjang kegiatan belajar mengajarmu, kami mencoba memberikan pelayanan terbaik untuk kamu
+            </p>
+            <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="assets/img/manager/tick.png"
+                width="35" alt="tick" />
+              <p class="fw-medium mb-0 text-secondary">Layanan Call Center yang akan membantu menyelesaikan masalahmu
+              </p>
+            </div>
+            <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="assets/img/manager/tick.png"
+                width="35" alt="tick" />
+              <p class="fw-medium mb-0 text-secondary">Ajukan pengembalian dana jika kamu merasa tidak puas dengan
+                layanan mentor kami</p>
+            </div>
+            <div class="d-flex align-items-center mb-3"><img class="me-sm-4 me-2" src="assets/img/manager/tick.png"
+                width="35" alt="tick" />
+              <p class="fw-medium mb-0 text-secondary">Mentor terverifikasi dengan pengalaman yang mumpuni
+              </p>
+            </div>
+          </div>
+        </div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="py-md-11 py-8" id="superhero">
+
+      <div class="bg-holder z-index--1 bottom-0 d-none d-lg-block background-position-top"
+        style="background-image:url(assets/img/superhero/oval.png);opacity:.5; background-position: top !important ;">
+      </div>
+      <!--/.bg-holder-->
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <h1 class="fw-bold mb-4 fs-7">Anda juga bisa menjadi mentor yang hebat</h1>
+            <p class="mb-5 text-info fw-medium">Bagikan pengetahuan dan passion Anda lalu jadilah seseorang yang mandiri
+            </p>
+            <button class="btn btn-warning btn-md">Bergabung Bersama Estudiar</button>
+          </div>
+        </div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+
+
+
+
+
+
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="pb-2 pb-lg-5">
+
+      <div class="container">
+        <div class="row border-top border-top-secondary pt-7">
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-6 mb-lg-0 mb-sm-2 order-1 order-md-1 order-lg-1"><img class="mb-4"
+              src="https://i.ibb.co/6yTVSfP/image-1.png" width="184" alt="" /></div>
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 order-3 order-md-3 order-lg-2">
+            <p class="fs-2 mb-lg-4">Quick Links</p>
+            <ul class="list-unstyled mb-0">
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">About us</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Blog</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Contact</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">FAQ</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 order-4 order-md-4 order-lg-3">
+            <p class="fs-2 mb-lg-4">Legal stuff</p>
+            <ul class="list-unstyled mb-0">
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Disclaimer</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Financing</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Privacy Policy</a></li>
+              <li class="mb-1"><a class="link-900 text-secondary text-decoration-none" href="#!">Terms of Service</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 col-6 mb-4 mb-lg-0 order-2 order-md-2 order-lg-4">
+            <p class="fs-2 mb-lg-4">
+              Ingin dapat info menarik dari kami</p>
+            <form class="mb-3">
+              <input class="form-control" type="email" placeholder="Email" aria-label="phone" />
+            </form>
+            <button class="btn btn-warning fw-medium py-1">Langganan Sekarang</button>
+          </div>
+        </div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+
+
+
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
+    <section class="text-center py-0">
+
+      <div class="container">
+        <div class="container border-top py-3">
+          <div class="row justify-content-between">
+            <div class="col-12 col-md-auto mb-1 mb-md-0">
+              <p class="mb-0">&copy; 2022 Estudiar</p>
+            </div>
+
+          </div>
+        </div>
+      </div><!-- end of .container-->
+
+    </section>
+    <!-- <section> close ============================-->
+    <!-- ============================================-->
+
+
+  </main>
+  <!-- ===============================================-->
+  <!--    End of Main Content-->
+  <!-- ===============================================-->
+
+
+  <!-- ===============================================-->
+  <!--    JavaScripts-->
+  <!-- ===============================================-->
+  <script src="vendors/@popperjs/popper.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.min.js"></script>
+  <script src="vendors/is/is.min.js"></script>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+  <script src="vendors/fontawesome/all.min.js"></script>
+  <script src="assets/js/theme.js"></script>
+
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&amp;family=Volkhov:wght@700&amp;display=swap"
+    rel="stylesheet">
 </body>
 
 </html>
